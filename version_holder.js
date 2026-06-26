559ffb3a13f16031a62c40f8f321bc06dbe5e104
SAJAYKRISHNA SREEBALA <sreebala.sajaykrishna@gmail.com>
2026-06-26 18:17:00 +0530

    released SRI04v1.0.0

--- version_holder.js
+++ version_holder.js
@@ -1,7 +1,7 @@
 
   document.addEventListener("DOMContentLoaded", () => {
-    const SITE_VERSION = "SRI04(Web)-v0.1.0";
-    const PUBLISHED_ON = "07 Feb 2026, 20:12:48 IST";
+    const SITE_VERSION = "SRI04(Web)-v1.0.24";
+    const PUBLISHED_ON = "26 June 2026, 18:16:48 IST";
 
     document.querySelectorAll(".site-version").forEach(el => {
       el.textContent = `Version: ${SITE_VERSION} | Published on: ${PUBLISHED_ON}`;
