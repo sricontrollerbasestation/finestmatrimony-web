496115ba63d1de4a538e9930a8a6d0e14df816ee
SAJAYKRISHNA SREEBALA <sreebala.sajaykrishna@gmail.com>
2026-08-11 21:17:30 +0530

    updated some details

--- version_holder.js
+++ version_holder.js
@@ -1,7 +1,7 @@
 
   document.addEventListener("DOMContentLoaded", () => {
-    const SITE_VERSION = "SRI04(Web)-v1.0.24";
-    const PUBLISHED_ON = "26 June 2026, 18:16:48 IST";
+    const SITE_VERSION = "SRI04(Web)-v2.5.32";
+    const PUBLISHED_ON = "11 August 2026, 21:16:00 IST";
 
     document.querySelectorAll(".site-version").forEach(el => {
       el.textContent = `Version: ${SITE_VERSION} | Published on: ${PUBLISHED_ON}`;
